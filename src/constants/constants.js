74be65b9cfed394c3fb6ffe0d0a64e97c4bd9b1b
Avantika-Ajay <122859842+Avantika-Ajay@users.noreply.github.com>
2025-03-06 12:57:12 +0530

    commit_2

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,3 +1,3 @@
 export const baseUrl='https://api.themoviedb.org/3';
-export const API_KEY='3b3c185aa9cfa140f71704beb3a7cd44';
+export const API_KEY='';
 export const imageURL='https://image.tmdb.org/t/p/original';
